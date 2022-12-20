795c7e658dd31f2af62861f7c3a2400625dff7c8
Adil Babaev <115106464+user5748@users.noreply.github.com>
2022-12-19 21:13:02 -0800

    first commit

---
+++ new task/js/index.js
@@ -0,0 +1,7 @@
+let games = [
+    ['Resident Evil', 'outlast', 'The Evil Within', 'Dying light', 'Silent Hill'],
+    ['Battlefield', 'Call of duty', 'Killzone', 'resistance', 'Pubg'],
+    ['Batman', 'The last Of Us', 'Devil May cry', 'Assassins Creed']
+]
+document.write(games[2][3][0] + games[0][0][4] + games[1][3][3] + games[2][2][4]);
+console.log(games[2][0][0] + games[1][0][1] + games[1][4][2] + games[1][0][1] + games[2][2][1] + games[0][2][5])
